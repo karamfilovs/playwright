2359a2375eb2a3560ab9a67e95b3f934abdf92bd
Aleksandar Karamiflov <aleksandar.karamfilov.extern@joyn.de>
2021-08-19 20:30:48 +0300

    Fixed broken e2e test

--- src/test/java/smoke/playwright/LoginPageTest.java
+++ src/test/java/smoke/playwright/LoginPageTest.java
@@ -52,7 +52,7 @@ public class LoginPageTest extends TestSetupPlayWright {
     @DisplayName("Can login with valid credentials - Playwright5")
     public void canLoginWithValidCredentials5() {
         app.loginPage
-                .populateUsername("karamfilov@gmail.com")
+                .populateUsername("karamfilovs@gmail.com")
                 .populatePassword("123456")
                 .pressLoginButton();
         Assertions.assertEquals("karamfilovs@gmail.com", app.homePage.getUserPanelText());
